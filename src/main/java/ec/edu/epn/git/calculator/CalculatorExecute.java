9e83f9409995c5f9d9613ebab04367531609d271
sebasG101 <sebastian.guerra01@epn.edu.ec>
2024-10-25 15:10:49 -0500

    Se realizó una modificación en la clase CalculatorExecute.

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -7,5 +7,8 @@ public class CalculatorExecute {
         System.out.println("c.addition(4,7) = " + addition);
         int subtraction = c.subtraction(7,1);
         System.out.println("c.subtraction(7,1) = " + subtraction);
+
+        System.out.println("✨ ¡Este código ha sido MODIFICADO! 🎉");
+
     }
 }
